a66c3a9f91278cd5602d991b066573419ede0035
BaicHome <82888929+BaicHome@users.noreply.github.com>
2022-04-30 06:49:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -119,10 +119,6 @@ install_XrayR() {
            echo -e "检测到 XrayR 最新版本：${last_version}，开始安装"
         wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip https://github.com/BaicHome/XrayR/releases/download/${last_version}/XrayR-linux-${arch}.zip
         fi
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 XrayR 失败，请确保你的服务器能够下载 Github 的文件${plain}"
-            exit 1
-        fi
     else
         last_version=$1
         url="https://github.com/BaicHome/XrayR/releases/download/${last_version}/XrayR-linux-${arch}.zip"
